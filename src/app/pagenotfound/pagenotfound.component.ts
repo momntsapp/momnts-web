2b017b1b71590e8edb2a76cdaa865598df6eed68
NathanArunaaa <nathanaruna07@gmail.com>
2023-10-15 12:38:30 -0400

    some seo cahnges

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SeoService } from '../seo.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pagenotfound',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagenotfound.component.css']
 })
 export class PagenotfoundComponent {
+  constructor(private seo: SeoService, private title: Title) {
+  }
 
+  ngOnInit(): void {
+
+    this.title.setTitle('Momnts | 404');
+
+    this.seo.generateTags({
+      title: 'Momnts | 404',
+      description: 'Momnts Social Media App by Domi, Nathan, Xin Lei & Aly',
+      image: '',
+      slug: 'home'
+    })
+
+}
 }
